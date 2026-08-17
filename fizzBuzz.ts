9ab416dde1c624ad1c725b20b36ad4bc3ec6262e
Benjamin Leray <b.leray35@gmail.com>
2026-08-17 15:38:13 +0200

    use rules collection

--- fizzBuzz.ts
+++ fizzBuzz.ts
@@ -1,16 +1,21 @@
-export const fizzBuzz = (number: number) => {
+const DEFAULT_RULES = [
+  { divisor: 3, output: "Fizz" },
+  { divisor: 5, output: "Buzz" },
+]
+type Rule = { divisor: number; output: string }
+
+export const fizzBuzz = (number: number, rules: Rule[] = DEFAULT_RULES) => {
   if (!number || !Number.isInteger(number)) {
     return
   }
   for (let i = 1; i <= number; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz")
-    } else if (i % 3 === 0) {
-      console.log("Fizz")
-    } else if (i % 5 === 0) {
-      console.log("Buzz")
-    } else {
-      console.log(i)
-    }
+    const result = rules.reduce((acc, rule) => {
+      if (i % rule.divisor === 0) {
+        acc += rule.output
+      }
+      return acc
+    }, "")
+
+    console.log(result || i)
   }
 }
