99ef64394eea75ef1932dbdba17bc6c8e0075881
Benjamin Leray <b.leray35@gmail.com>
2026-08-17 15:51:18 +0200

    cleanup

--- fizzBuzz.ts
+++ fizzBuzz.ts
@@ -1,21 +1,25 @@
+type Rule = { divisor: number; output: string }
+
 const DEFAULT_RULES = [
   { divisor: 3, output: "Fizz" },
   { divisor: 5, output: "Buzz" },
 ]
-type Rule = { divisor: number; output: string }
 
 export const fizzBuzz = (number: number, rules: Rule[] = DEFAULT_RULES) => {
   if (!number || !Number.isInteger(number)) {
     return
   }
-  for (let i = 1; i <= number; i++) {
-    const result = rules.reduce((acc, rule) => {
-      if (i % rule.divisor === 0) {
-        acc += rule.output
-      }
-      return acc
-    }, "")
 
-    console.log(result || i)
+  for (let i = 1; i <= number; i++) {
+    console.log(numberToFizzBuzz(i, rules) || i)
   }
+}
+
+const numberToFizzBuzz = (number: number, rules: Rule[]) => {
+  return rules.reduce((acc, rule) => {
+    if (number % rule.divisor === 0) {
+      acc += rule.output
+    }
+    return acc
+  }, "")
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { fizzBuzz } from "./fizzBuzz.ts"
 const run = (input: any) => {
   const userNumber = Number(input)
+
   if (isNaN(userNumber)) {
     console.log("Please provide a valid number as an argument.")
     process.exit(1)
