02d47146d1b66b529a2a46ec7a698c248d90bbcb
Benjamin Leray <b.leray35@gmail.com>
2026-08-18 17:17:16 +0200

    temporary ui

--- user_search/src/App.tsx
+++ user_search/src/App.tsx
@@ -1,7 +1,36 @@
-import './App.css'
+import { useState, type ChangeEvent } from "react"
+import "./App.css"
+import { getSearchUsers } from "./hooks/getSearchUsers"
 
 function App() {
-  return <h1>Hello world</h1>
+  const [queryParams, setQueryParams] = useState("")
+
+  const { users, totalCount } = getSearchUsers({ query: queryParams })
+
+  const handleOnUserSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQueryParams(e.target.value)
+  }
+  return (
+    <>
+      <h1>Hello World</h1>
+      <form>
+        <label htmlFor="user-search">Search</label>
+        <input
+          id="user-search"
+          type="text"
+          placeholder="search..."
+          onChange={handleOnUserSearchChange}
+        />
+      </form>
+      <p>number of result : {totalCount}</p>
+      <p>list of users:</p>
+      <ul>
+        {users.map((user) => {
+          return <li>{user.id}</li>
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default App
