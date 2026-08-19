a4de04d664d14858cd734535f18eca034c2f091b
Benjamin Leray <b.leray35@gmail.com>
2026-08-19 12:08:01 +0200

    add card

--- user_search/src/components/UserList.tsx
+++ user_search/src/components/UserList.tsx
@@ -1,3 +1,15 @@
+import { Card } from "./Card"
+import { mockUsers } from "../test/mocks.ts"
 export const UserList = () => {
-  return <div>UserList</div>
+  const user = mockUsers[0]
+  return (
+    <div>
+      <Card>
+        <Card.Image src={user.avatar_url} alt={`${user.login} avatar`} />
+        <Card.Label text={user.id} />
+        <Card.Label text={"super long name yough"} />
+        <Card.Action href={user.url} label="View Profile" />
+      </Card>
+    </div>
+  )
 }
