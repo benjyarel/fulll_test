6d4fb87d24c67d3c8f7f4dc1ab10d089090b803a
Benjamin Leray <b.leray35@gmail.com>
2026-08-19 11:28:28 +0200

    add icons

--- user_search/src/components/Toolbox.tsx
+++ user_search/src/components/Toolbox.tsx
@@ -1,3 +1,19 @@
+import { CopyIcon } from "./icons/CopyIcon"
+import { TrashIcon } from "./icons/TrashIcon"
+import style from "./Toolbox.module.css"
+
 export const Toolbox = () => {
-  return <div>toolbox</div>
+  return (
+    <div className={style.toolbox}>
+      <div> X elements selected</div>
+      <div className={style.actions}>
+        <button type="button" aria-label="Duplicate selection">
+          <CopyIcon />
+        </button>
+        <button type="button" aria-label="Delete selection">
+          <TrashIcon />
+        </button>
+      </div>
+    </div>
+  )
 }
